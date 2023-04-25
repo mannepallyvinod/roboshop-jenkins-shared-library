000ad3bca1d7ec0c6f66c7e3f31e01e98be064fb
mannepallyvinod <111588987+mannepallyvinod@users.noreply.github.com>
2023-04-25 11:49:36 +0530

    Add files via upload

---
+++ vars/common.groovy
@@ -0,0 +1,46 @@
+def compile() {
+    if(app_lang == "nodejs") {
+        sh 'npm install'
+    }
+    if(app_lang == "maven") {
+        sh 'mvn package'
+    }
+}
+
+
+def testcases() {
+    // npm test
+    // mvn test
+    // python -m unittests
+    // go test
+    sh 'echo OK'
+}
+
+
+def codequality() {
+    withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
+        //sh 'sonar-scanner -Dsonar.host.url=http://172.31.13.184:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts} -Dsonar.qualitygate.wait=true'
+        sh 'echo OK'
+    }
+}
+
+def prepareArtifacts() {
+    sh 'echo ${TAG_NAME} >VERSION'
+    if (app_lang == "maven") {
+        sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
+    } else {
+        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
+    }
+
+}
+
+def artifactUpload() {
+    env.NEXUS_USER = sh ( script: 'aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+    env.NEXUS_PASS = sh ( script: 'aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+    wrap([$class: 'MaskPasswordsBuildWrapper',
+          varPasswordPairs: [[password: NEXUS_PASS],[password: NEXUS_USER]]]) {
+        sh 'echo ${TAG_NAME} >VERSION'
+        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.4.97:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+
+    }
+}
---
+++ vars/terraform.groovy
@@ -0,0 +1,39 @@
+def call() {
+    pipeline {
+        agent any
+
+        parameters {
+            string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+            string(name: 'ACTION', defaultValue: '', description: 'Which Action?')
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+
+        stages {
+
+            stage('Init') {
+                steps {
+
+                    sh 'terraform init -backend-config=env-prod/state.tfvars'
+                }
+            }
+
+
+            stage('Apply') {
+                steps {
+                    sh 'terraform ${ACTION} -auto-approve -var-file=env-prod/main.tfvars'
+                }
+            }
+
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+}
+
